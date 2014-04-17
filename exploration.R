59795b54d8f1e95e31f3b94cb43b7549a17ef240
vtn6 <vtn6@cornell.edu>
2014-04-17 12:22:07 -0400

    Determined the different number of patients
    Figuring out how many different kind of patients there are in the
    stratal binding ratio table

--- exploration.R
+++ exploration.R
@@ -4,3 +4,11 @@ rm(list = ls())
 # Data Preprocess ---------------------------------------------------------
 
 setwd("~/Documents/Cornell/ORIE4740/FinalProject//parkinsonsDataMining")
+codeList = read.csv("ppmiData/_Study_Docs/Code_List.csv")
+dataDictionary = read.csv("ppmiData/_Study_Docs/Data_Dictionary.csv")
+striatal_Binding_Ratio_Results = read.csv("ppmiData/Imaging/DaTscan_Striatal_Binding_Ratio_Results.csv")
+
+head(striatal_Binding_Ratio_Results)
+
+number_of_patients = length(levels(as.factor(striatal_Binding_Ratio_Results$PATNO)))
+
---
+++ usefulFunctions.R
@@ -0,0 +1,5 @@
+
+
+# Useful functions for searching through Code List and Data Dictio --------
+#
+  dataDictionary[grep('ORIG_ENTRY',dataDictionary$ITM_NAME),]
